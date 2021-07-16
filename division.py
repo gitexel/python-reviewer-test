ea2dd8f0e8a60db0c3916a411acec062ef02efbc
Mohamed Salah <mo.salah@knowledgeofficer.com>
2021-07-16 17:09:51 +0200

    hot code fix

--- division.py
+++ division.py
@@ -1,27 +1,30 @@
-#Lab-2.py
+# Lab-2.py
 def main():
     operations_count = 0
     ask_again = True
-    while(ask_again):
+    while ask_again:
         a = input("Enter the numerator: ")
         b = input("Enter the denominator: ")
-        result = perform_division(a,b)
-        if(result is not None):
+        result = perform_division(a, b)
+        if result is not None:
             operations_count += 1
             print(result)
             ask_again = input("Do you want to perform another operation? Enter yes or no: ")
-            if(ask_again == 'yes'):
+            if ask_again == 'yes':
                 ask_again = True
             else:
                 ask_again = False
                 print("You performed " + str(operations_count) + " operations, bye!")
-def perform_division(a,b):
+
+
+def perform_division(a, b):
     try:
-        return int(a)/int(b)
+        return int(a) / int(b)
     except ZeroDivisionError as error:
         print("Looks like you are trying to divide by zero! This is not possible.")
     except Exception as error:
         print("Invalid numbers.")
 
 
-main()
+if __name__ == '__main__':
+    main()
